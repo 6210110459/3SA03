3d7966604e885c31207d1073220e4500f45a2339
6210110459 <6210110459@psu.ac.th>
2021-07-10 21:13:31 +0700

    card-game2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ const i = Math.floor(Math.random() * word.length);
 
 function App() {
   return (
-  <div >
+  <div>
+    <center> <img src="https://s.isanook.com/jo/0/rp/rc/w300h300/ya0xa0m1w0/aHR0cDovL2ltYWdlLmpvb3guY29tL0pPT1hjb3Zlci8wLzBjMTVjMmVlMjNlZmEzNTIvMTAwMC5qcGc=.jpg" widht="100px" height="100px"/> </center>
     <WordCard value={word[i]}/>
   </div>
   );
