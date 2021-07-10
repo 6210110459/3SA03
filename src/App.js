89901bd37f4fb343e102c7cd0378e647871870d9
6210110459 <6210110459@psu.ac.th>
2021-07-10 21:15:32 +0700

    card-game3

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 import WordCard from './WordCard';
 
 const word = ["Hello", "NiHao", "Konnichiwa", " Hola", "Aloha"];
-const i = Math.floor(Math.random() * word.length);
-
+const i = Math.floor(Math.random() * 5);
+const photo = ["http://www.thaifly.com/image/data/img_thaifly/article/england/infoengland.jpg2.jpg", "https://s.isanook.com/ca/0/ud/277/1387785/greatwall.jpg", "http://www.thaifly.com/image/data/img_thaifly/article/Japan/sakuranight.jpg", "https://costadefuego.com/wp-content/uploads/2019/10/%E0%B8%82%E0%B9%89%E0%B8%AD%E0%B8%A1%E0%B8%B9%E0%B8%A5%E0%B8%9B%E0%B8%A3%E0%B8%B0%E0%B9%80%E0%B8%97%E0%B8%A8%E0%B8%AA%E0%B9%80%E0%B8%9B%E0%B8%99.jpg", "https://my.kapook.com/imagescontent/fb_img/804/s_68600_7933.jpg" ];
 
 function App() {
   return (
   <div>
-    <center> <img src="https://s.isanook.com/jo/0/rp/rc/w300h300/ya0xa0m1w0/aHR0cDovL2ltYWdlLmpvb3guY29tL0pPT1hjb3Zlci8wLzBjMTVjMmVlMjNlZmEzNTIvMTAwMC5qcGc=.jpg" widht="100px" height="100px"/> </center>
+    <center> <img src={photo[i]}/> </center>
     <WordCard value={word[i]}/>
   </div>
   );
